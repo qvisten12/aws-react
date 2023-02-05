0e159e63b047b4c7cccb234b024779b89d685546
FelixLjungqvist <felixljungqvist97@gmail.com>
2023-02-05 18:01:43 +0100

    Fix vite errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import resolve from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...resolve({
+        preferBuiltins: false,
+        browser: true,
+      }),
+      enforce: 'pre',
+      apply: 'build',
+    },
+  ],
 })
