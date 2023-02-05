a9f83e75a12185af3109e97471bc55ca7e27c2d9
FelixLjungqvist <felixljungqvist97@gmail.com>
2023-02-05 12:02:04 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>Hello from V2.</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
